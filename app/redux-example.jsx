2576dc92a39f48f2dc65bd74d9342b3d05ed7a00
Ian Moore <ianmooregis@gmail.com>
2017-05-18 14:28:39 -0400

    Added async reducer and action for Google maps

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
 var redux = require('redux');
+var axios = require('axios');
 
 console.log('Starting redux example');
 
@@ -90,11 +91,54 @@ var removeMovie = (id) => {
     };
 };
 
+//Map reducer and action generators
+//--------------------
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+    switch (action.type) {
+        case 'START_LOCATION_FETCH':
+            return {
+                isFetching: true,
+                url: undefined
+            };
+        case 'COMPLETE_LOCATION_FETCH':
+            return {
+                isFetching: false,
+                url: action.url
+            };
+        default:
+            return state;
+    }
+        
+};
+
+var startLocationFetch = () => {
+    return {
+        type: 'START_LOCATION_FETCH'
+    };
+};
+
+var completeLocationFetch = (url) => {
+    return {
+        type: 'COMPLETE_LOCATION_FETCH',
+        url
+    };
+}
+
+var fetchLocation = () => {
+    store.dispatch(startLocationFetch());
+    axios.get('http://ipinfo.io').then((res)=> {
+        var loc = res.data.loc;
+        var baseUrl = 'http://maps.google.com/?q=';
+        store.dispatch(completeLocationFetch(baseUrl + loc));
+    });
+};
+
 //Consolidate into one reducer
 var reducer = redux.combineReducers({
     name: nameReducer,
     hobbies: hobbiesReducer,
-    movies: moviesReducer
+    movies: moviesReducer,
+    map: mapReducer
 });
 
 //check for Redux dev Chrome extension: if it does not exist, pass plain function to keep middleware process working
@@ -106,10 +150,13 @@ var store = redux.createStore(reducer, redux.compose(
 var unsubscribe = store.subscribe(()=> {
     var state = store.getState();
     
-    console.log('Name is', state.name);
-    document.getElementById('app').innerHTML = state.name;
-    
     console.log('New state', store.getState());
+    
+    if (state.map.isFetching) {
+        document.getElementById('app').innerHTML = 'Loading...';
+    } else if (state.map.url) {
+        document.getElementById('app').innerHTML = '<a href="' + state.map.url + '" target="_blank">View your location</a>';
+    }
 });
 //unsubscribe();
 
@@ -117,6 +164,8 @@ var currentState = store.getState();
 console.log('currentState', currentState);
 
 //dispatch actions
+fetchLocation();
+
 store.dispatch(changeName('Andrew'));
 
 store.dispatch(addHobby('Running'));
